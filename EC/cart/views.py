49c605876fb8c9957747ff0ff1516b11b9e06a68
Shirai <taole33@hotmail.com>
2020-08-15 16:01:57 +0900

    Update views.py

--- EC/cart/views.py
+++ EC/cart/views.py
@@ -2,9 +2,11 @@ from django.http import Http404, HttpResponseRedirect
 from django.urls import reverse
 from django.template.response import TemplateResponse
 from django.views.decorators.http import require_POST
+from django.shortcuts import render
+from django.views.generic import View
 
 from merchandise.models import Product
-
+import requests
 
 @require_POST
 def cart_add(request, product_id):
@@ -35,6 +37,7 @@ def cart_list(request):
     total_price = 0
     for product in products:
         total_price += product.price
+    request.session['total_price'] = total_price
     
     return TemplateResponse(request, 'cart/list.html',
                             {'products': products,
@@ -51,3 +54,19 @@ def cart_delete(request,product_id):
         request.session['cart'] = filtered
     return HttpResponseRedirect(reverse('cart_list'))
 
+
+@require_POST
+def payment(request):
+    total_price = request.session.get('total_price')
+    payjp_token = request.POST.get('payjp-token')
+
+    request_url = 'https://api.pay.jp/v1/charges'
+    params = {
+        'amount': total_price,
+        'currency': 'jpy',
+        'card': payjp_token
+        }
+
+    requests.post(request_url, params, auth=("WRITE_HERE_secretkey", ""))
+    request.session['cart'] = None
+    return render(request, 'cart/pay_done.html',{'total_price':total_price} )

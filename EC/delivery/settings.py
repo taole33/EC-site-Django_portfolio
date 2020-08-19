c69f5aba20afdc188dd6408510f7da85479f3bbb
Shirai <taole33@hotmail.com>
2020-08-19 21:40:12 +0900

    Update settings.py

--- EC/delivery/settings.py
+++ EC/delivery/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i-a^seb^38p_7mf7x)t24=$d3*dyx#)h4l#h^+n*$_d0)=d4@8'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
